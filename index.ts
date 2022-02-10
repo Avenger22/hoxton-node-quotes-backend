d495a2a513151f1c8bc365c304808ddfe049e6b5
Jurgen Hasmeta <jurgenhasmeta12@yahoo.com>
2022-02-10 16:14:53 +0100

    backend finished

---
+++ index.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import {
+    quotes
+} from "./db/db"
+
+const app = express();
+const PORT: number = 8000;
+
+app.get('/', (req, res) => {
+    res.send('Welcome to AlbVitaFitness!!!!, use / and arrays for navigation in the backend server');
+});
+
+app.get('/quotes', (req, res) => {
+    res.send(quotes);
+});
+
+app.get('/quotes/:author', (req, res) => {
+
+    const author = String(req.params.author)
+    const match = quotes.find((quote) => quote.author === author)
+    
+    if (match) {
+      res.send(match)
+    } 
+    
+    else {
+      res.status(404).send({ error: 'Quote not found.' })
+    }
+  
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on: http://localhost:${PORT}`);
+})
